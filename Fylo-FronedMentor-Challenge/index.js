c7bbc4dba9a0d69b47a72bce2ebaaeb4451c115c
Tal Mekler <tal.mekler11@gmail.com>
2022-12-11 21:01:01 +0200

    fix

--- Fylo-FronedMentor-Challenge/index.js
+++ Fylo-FronedMentor-Challenge/index.js
@@ -1,48 +1,54 @@
-const uploadBtn = document.getElementById("upload-file-btn");
 const sizeUnitElements = document.getElementsByClassName("size-unit");
+const uploadedFilesWrapper = document.getElementById("uploaded-files-wrapper");
 
 for (var i = 0; i < sizeUnitElements.length; i++) {
   sizeUnitElements[i].innerHTML = "MB";
 }
 const totalSize = 100;
-const maxSizeElement = document.getElementById("max-size");
-let usedSize = 0;
-const usedSizeElement = document.getElementById("used-size");
-const remainSizeElement = document.getElementById("remain-size");
-const progressBarClr = document.getElementById("progress-bar-clr");
-const progressBarCircle = document.getElementById("progress-bar-circle");
-const uploadedFilesWrapper = document.getElementById("uploaded-files-wrapper");
 
+let usedSize = parseFloat(Number(window.localStorage.getItem("usedSize")).toFixed(2));
 
 // window.localStorage.setItem("usedSize", 0);
 
-updateSizes();
-uploadBtn.addEventListener("change", () => {
-  let fileName = uploadBtn.value.split("\\");
+function onFileInputChange(event) {
+  let fileName = event.value.split("\\");
   fileName = fileName[fileName.length - 1];
-  let validFile = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(fileName);
-  console.log(validFile);
-  if (!validFile) {
+
+  const isImgFile = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(fileName);
+  if (!isImgFile) {
     alert("Invalid file type!");
   } else {
-    let fileSize = uploadBtn.files[0];
-    fileSize = parseFloat(parseFloat(fileSize.size / 1000 / 1000).toFixed(2));
-    usedSize += fileSize;
-    if (usedSize > totalSize) {
+    // let fileSize = event.files[0];
+    let fileSize = parseFloat(parseFloat(event.files[0].size / 1024 / 1024).toFixed(2));
+    if (usedSize + fileSize > totalSize) {
       alert("Not enough size!");
-      usedSize -= fileSize;
+      // usedSize -= fileSize;
     } else {
+      usedSize += fileSize;
       window.localStorage.setItem("usedSize", usedSize);
       updateSizes();
       uploadedFilesWrapper.innerHTML +=
-        '<div class="uploaded-file__item" file-size="'+fileSize+'"><p class="uploaded-file__item-text"><span>'+ fileName +'</span><i class="fa-solid fa-xmark" onclick="removeItem(this)"></i></p></div>';
+        '<div class="uploaded-file__item" file-size="' +
+        fileSize +
+        '"><p class="uploaded-file__item-text"><span>' +
+        fileName +
+        '</span><i class="fa-solid fa-xmark" onclick="removeItem(this)"></i></p></div>';
     }
   }
-  uploadBtn.value = "";
-});
+  event.value = "";
+}
+
+updateSizes();
 
 function updateSizes() {
-  usedSize = parseFloat(JSON.parse(window.localStorage.getItem("usedSize")).toFixed(2));
+  const usedSizeElement = document.getElementById("used-size");
+  const remainSizeElement = document.getElementById("remain-size");
+  const progressBarClr = document.getElementById("progress-bar-clr");
+  const progressBarCircle = document.getElementById("progress-bar-circle");
+  const maxSizeElement = document.getElementById("max-size");
+  
+
+  // usedSize = parseFloat(Number(window.localStorage.getItem("usedSize")).toFixed(2));
   usedSizeElement.innerText = usedSize.toFixed(2);
   remainSizeElement.innerHTML = (totalSize - usedSize).toFixed(2);
   maxSizeElement.innerText = totalSize;
@@ -58,11 +64,12 @@ document.getElementById("zero-size").addEventListener("click", () => {
 
 function reset() {
   uploadedFilesWrapper.innerHTML = "";
+  usedSize = 0;
   window.localStorage.setItem("usedSize", 0);
   updateSizes();
 }
 
-function removeItem(item){
+function removeItem(item) {
   const fileItemDiv = item.parentNode.parentNode;
   usedSize -= parseFloat(fileItemDiv.getAttribute("file-size"));
   window.localStorage.setItem("usedSize", usedSize);
