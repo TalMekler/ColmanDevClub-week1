5c8b93a61ef5a28f5a1211eca700074f4cf15aa6
Tal Mekler <tal.mekler11@gmail.com>
2022-12-11 20:10:47 +0200

    fix vars to let and const

--- Fylo-FronedMentor-Challenge/index.js
+++ Fylo-FronedMentor-Challenge/index.js
@@ -1,32 +1,31 @@
-var uploadBtn = document.getElementById("upload-file-btn");
-var sizeUnitElements = document.getElementsByClassName("size-unit");
+const uploadBtn = document.getElementById("upload-file-btn");
+const sizeUnitElements = document.getElementsByClassName("size-unit");
 
 for (var i = 0; i < sizeUnitElements.length; i++) {
   sizeUnitElements[i].innerHTML = "MB";
 }
-var totalSize = 100;
+const totalSize = 100;
 const maxSizeElement = document.getElementById("max-size");
-var usedSize = 0;
+let usedSize = 0;
 const usedSizeElement = document.getElementById("used-size");
 const remainSizeElement = document.getElementById("remain-size");
 const progressBarClr = document.getElementById("progress-bar-clr");
 const progressBarCircle = document.getElementById("progress-bar-circle");
-var uploadedFilesWrapper = document.getElementById("uploaded-files-wrapper");
+const uploadedFilesWrapper = document.getElementById("uploaded-files-wrapper");
 
-var fileName;
-var validFile;
-var fileSize;
+
 // window.localStorage.setItem("usedSize", 0);
+
 updateSizes();
 uploadBtn.addEventListener("change", () => {
-  fileName = uploadBtn.value.split("\\");
+  let fileName = uploadBtn.value.split("\\");
   fileName = fileName[fileName.length - 1];
-  validFile = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(fileName);
+  let validFile = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(fileName);
   console.log(validFile);
   if (!validFile) {
     alert("Invalid file type!");
   } else {
-    fileSize = uploadBtn.files[0];
+    let fileSize = uploadBtn.files[0];
     fileSize = parseFloat(parseFloat(fileSize.size / 1000 / 1000).toFixed(2));
     usedSize += fileSize;
     if (usedSize > totalSize) {
